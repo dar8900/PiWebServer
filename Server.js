6ce997e199c83ad9ff783b037309222cf7ee1960
Dario Acer V_NITRO <dar8900@gmail.com>
2020-12-07 00:05:47 +0100

    -Inserite le prime funzioni asincrone per la lettura della temperatura -Creato un server di prova

--- Server.js
+++ Server.js
@@ -1,18 +1,59 @@
+
+class Error 
+{
+    constructor(CodeNumber, Motivation) 
+    {
+        this.CodeNumber = CodeNumber;
+        this.Motivation = Motivation;
+    }
+    getErrorObject() 
+    {
+        var error = {en: this.CodeNumber, why: this.Motivation};
+        return error;
+    }
+}
 const TEMP_SCRIPT = './TempScript.sh'
 const SERVER_PORT = 1989
 
 var http = require('http');
 
-http.createServer(function (req, res) {
-    const exec = require('child_process').exec;
-    const myShellScript = exec(TEMP_SCRIPT);
-    myShellScript.stdout.on('data', (data)=>{ 
-        let newdata = (parseFloat(data)/1000.0).toFixed(1).toString();
-        console.log(newdata);
+function getTemp()
+{
+    console.log("GetTemp Started");
+    return new Promise((resolve, reject) =>{
+        const ScriptRun = require('child_process');
+        const TempOut = ScriptRun.spawn(TEMP_SCRIPT);
+        TempOut.stdout.on("data", (data) => {
+            resolve((parseFloat(data)/1000.0).toFixed(1).toString() + "°C");
+        });
+        TempOut.on("error", (err) => {
+            let newError = new Error(1, "Script Error")
+            resolve(newError.getErrorObject());
+        });
     });
-    myShellScript.stderr.on('data', (data)=>{
-        console.error(data);
+}
+
+async function readTemp()
+{
+    console.log("ReadTemp Started");
+    let tempReaded = await getTemp();
+    return new Promise((resolve, reject) =>{
+        resolve(tempReaded);
     });
+}
+
+
+http.createServer((req, res) => {
+    
+    async function showTemp()
+    {
+        let temp;
+        temp = await readTemp();            
+        console.log(temp);
+        return temp;
+    }
+    
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end("Cpu temp: " + showTemp() + "°C");
+
 }).listen(SERVER_PORT);
