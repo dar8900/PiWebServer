797b9f9f3f012bc9386baa2dcbe9e912e1dd7305
PiServer <piserver.ubuntu@gmail.com>
2020-12-14 16:57:33 +0100

    Eliminate le pagine web statiche Aggiunto gitingore

--- HtmlPages/Scripts/index.js
+++
@@ -1,9 +0,0 @@
-const { user } = require('../../Server.js');
-
-var username = require('../../Server.js').user;
-function writeUsername()
-{
-    let Welcome = document.getElementById("welcome_title");
-    Welcome.innerHTML += username;
-    console.log("Script di index.html");
-}
